9f9d9398b4f1e622b6dec4da629deb9ba2f8fab0
John Druhan <jfrancisdruhan@comcast.net>
2019-06-09 18:51:34 -0700

    update

--- models/example.js
+++ models/example.js
@@ -1,7 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
   var Signup = sequelize.define("Signup", {
-    // text: DataTypes.STRING,
-    // description: DataTypes.TEXT
 
     name: DataTypes.STRING,
     email: DataTypes.TEXT
